0e693a6f13fa5e1e17ee773ae26e0c574de2d8a2
isro <mohammadisro2710@gmail.com>
2025-09-06 22:52:22 +0700

    fix: swiper css

--- tamagosui-ui/src/main.tsx
+++ tamagosui-ui/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import "swiper/css"
-import 'swiper/css/effect-cards';
+import "swiper/css/effect-cards"
 import App from "./App.tsx"
 
 createRoot(document.getElementById("root")!).render(
